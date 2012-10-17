865a3ee1cd1acddebbfbbe5ffe795610b20cf342
Deconinck <137479@supinfo.com>
2012-10-18 00:28:21 +0200

    Remove

---
+++ include/nav.php
@@ -0,0 +1,40 @@
+<nav class="span3">
+	<div class="row offset1">
+		<img src ="http://www.placehold.it/240x200" />
+	</div>
+	<div class="row">
+		<ul>
+			<a href="index.php">
+				<li>Accueil</li>
+			</a>
+			<a href="competences.php">
+				<li>Compétences</li>
+			</a>
+			<a href="realisations.php">
+				<li>Réalisations</li>
+			</a>
+			<a href="experiences.php">
+				<li>Expériences</li>
+			</a>
+			<a href="contact.php">
+				<li>Contact</li>
+			</a>
+		</ul>
+	</div>
+	<div class="row">
+		<ul class="social">
+			<li>
+				<img src ="http://www.placehold.it/50x50" />
+			</li>
+			<li>
+				<img src ="http://www.placehold.it/50x50" />
+			</li>
+			<li>
+				<img src ="http://www.placehold.it/50x50" />
+			</li>
+			<li>
+				<img src ="http://www.placehold.it/50x50" />
+			</li>
+		</ul>
+	</div>
+</nav>	
---
+++ index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+		<meta charset="utf-8">
+		<title>Deconinck Thomas - Développeur web et mobile</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta name="description" content="Portfolio d'un étudiant à SUPINFO passionnée par le développement web et mobile.">
+		<meta name="author" content="Deconinck Thomas">
+
+		<link href="css/main.css" rel="stylesheet" />
+		<link href="js/google-code-prettify/prettify.css" rel="stylesheet" />
+
+		<!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
+		<!--[if lt IE 9]>
+			<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+		<![endif]-->
+
+
+	</head>
+
+	<body data-spy="scroll" data-target=".bs-docs-sidebar">
+	
+	<div class="container-fluid">
+		<div class="row-fluid">
+			<?php require_once('include/nav.php'); ?>
+			<div data-spy="scroll" class="span8 offset3 home">
+				<header>
+					<h1>Développeur web et mobile</h1>
+				</header>
+				<div class="row">
+					<div class="offset1 presentation">
+						<img src ="img/presentation/imac.png" />
+						<img src ="img/presentation/ipad.png" />
+						<img src ="img/presentation/iphone.png" />
+					</div>
+				</div>
+				
+				<p class="full-screen">
+					<img src="img/presentation/dck.png" class="img-polaroid" alt="Deconinck Thomas" />
+					Etudiant en 2<sup>ème</sup> année à SUPINFO Lille. <br />Je suis passionné par l'informatique depuis mon plus bas âge, j'ai donc mis la priorité la dessus et j'ai obtenu les bases (hardware et software) au Collège Technique St Joseph en faisant un BAC <em>technicien informaticien</em> mais c'est pendant ces études que j'ai découvert par moi-même la programmation et les technologies du web.
+					<br /><br />
+					Vous pourrez trouver à travers ce portfolio mes diverses compétences, réalisations, mais également mes expériences. N'hésitez pas à me suivre sur les réseaux sociaux !
+				</p>
+			</div>
+		</div>
+	</div>
+
+
+
+
+
+
+		<script type="text/javascript" src="http://platform.twitter.com/widgets.js"></script>
+		<script src="js/jquery.js"></script>
+		<script src="js/google-code-prettify/prettify.js"></script>
+		<script src="js/bootstrap-transition.js"></script>
+		<script src="js/bootstrap-alert.js"></script>
+		<script src="js/bootstrap-modal.js"></script>
+		<script src="js/bootstrap-dropdown.js"></script>
+		<script src="js/bootstrap-scrollspy.js"></script>
+		<script src="js/bootstrap-tab.js"></script>
+		<script src="js/bootstrap-tooltip.js"></script>
+		<script src="js/bootstrap-popover.js"></script>
+		<script src="js/bootstrap-button.js"></script>
+		<script src="js/bootstrap-collapse.js"></script>
+		<script src="js/bootstrap-carousel.js"></script>
+		<script src="js/bootstrap-typeahead.js"></script>
+		<script src="js/bootstrap-affix.js"></script>
+		<script src="js/application.js"></script>
+	</body>
+</html>
