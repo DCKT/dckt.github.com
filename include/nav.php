d5d284860d308bb638366c91a8d848a0285f78c0
Deconinck <137479@supinfo.com>
2012-10-18 11:17:12 +0200

    Add responsive

--- include/nav.php
+++ include/nav.php
@@ -1,4 +1,4 @@
-<nav class="span3">
+<nav class="span3 menu hidden-phone">
 	<div class="row offset1">
 		<img src ="http://www.placehold.it/240x200" />
 	</div>
@@ -37,4 +37,44 @@
 			</li>
 		</ul>
 	</div>
-</nav>	
+</nav>
+
+<!-- <nav class="menu-phone visible-phone">
+	<ul>
+		<a href="index.php">
+			<li>Accueil</li>
+		</a>
+		<a href="competences.php">
+			<li>Compétences</li>
+		</a>
+		<a href="realisations.php">
+			<li>Réalisations</li>
+		</a>
+		<a href="experiences.php">
+			<li>Expériences</li>
+		</a>
+		<a href="contact.php">
+			<li>Contact</li>
+		</a>
+	</ul>
+	<ul class="nav nav-pills">
+	  <li class="active">
+	    <a href="#">Home</a>
+	  </li>
+	  <li><a href="#">Compétences</a></li>
+	  <li><a href="#">Réalisations</a></li>
+	  <li><a href="#">...</a></li>
+	</ul>
+</nav>	 -->
+<nav class="menu-phone navbar navbar-fixed-bottom visible-phone">
+  <div class="navbar-inner">
+    <a class="brand" href="#"></a>
+    <ul class="nav">
+      <li class="active"><a href="index.php">Home</a></li>
+      <li><a href="#">Compétences</a></li>
+      <li><a href="#">Réalisations</a></li>
+      <li><a href="#">Expériences</a></li>
+      <li><a href="contact.php">Contact</a></li>
+    </ul>
+  </div>
+</nav>
